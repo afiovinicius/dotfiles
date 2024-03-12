d8c62e58ebddc0fac38fe289587e677378350a8c
afiovinicius <afiovinicius@gmail.com>
2024-03-12 00:30:12 -0300

    Nem Cristo sabe que bomba e essa imagina eu

---
+++ .zshrc
@@ -0,0 +1,39 @@
+# Path to your oh-my-zsh installation.
+export ZSH="$HOME/.oh-my-zsh"
+
+# See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
+ZSH_THEME="spaceship"
+# jispwoso ; kafeitu ; juanghurtado
+
+plugins=(
+    git
+    zsh-autosuggestions
+    zsh-completions
+    zsh-syntax-highlighting
+)
+
+source $ZSH/oh-my-zsh.sh
+
+SPACESHIP_PROMPT_ORDER=(
+  user          # Username section
+  dir           # Current directory section
+  host          # Hostname section
+  git           # Git section (git_branch + git_status)
+  hg            # Mercurial section (hg_branch  + hg_status)
+  python        # Python section
+  node          # Node section
+  venv           # virtualenv section
+  exec_time     # Execution time
+  line_sep      # Line break
+  jobs          # Background jobs indicator
+  exit_code     # Exit code section
+  char          # Prompt character
+)
+SPACESHIP_USER_SHOW=always
+SPACESHIP_PROMPT_ADD_NEWLINE=false
+SPACESHIP_CHAR_SYMBOL="❯"
+SPACESHIP_CHAR_SUFFIX=" "
+
+
+# Load Angular CLI autocompletion.
+source <(ng completion script)
---
+++ setup.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+
+# Atualização do sistema
+sudo pacman -Syu
+
+# Ajustando SWAP
+sudo sysctl -w vm.swappiness=10
+sysctl -a | grep -i swappiness
+
+# Configuração das fonts e emojis
+sudo pacman -S --noconfirm noto-fonts-emoji adobe-source-code-pro-fonts adobe-source-serif-fonts adobe-source-sans-fonts ttf-inconsolata
+sudo mkdir ~/.config/fontconfig
+echo -e '<?xml version="1.0" encoding="UTF-8"?>\n<!DOCTYPE fontconfig SYSTEM "fonts.dtd">\n<fontconfig>\n
+<!-- ## serif ## -->\n  <alias>\n               <family>serif</family>\n                <prefer>\n                      <family>Noto Serif</family>\n                     <family>emoji</family>\n                        <family>Liberation Serif</family>\n
+        <family>Nimbus Roman</family>\n                 <family>DejaVu Serif</family>\n         </prefer>\n     </alias>\n      <!-- ## sans-serif ## -->\n       <alias>\n               <family>sans-serif</family>\n           <prefer>\n                      <family>Noto Sans</family>\n                      <family>emoji</family>\n                        <family>Liberation Sans</family>\n                        <family>Nimbus Sans</family>\n                  <family>DejaVu Sans</family>\n          </prefer>\n     </alias>\n</fontconfig>' > /home/$USER/.config/fontconfig/fonts.conf
+
+
+echo -n "Instalações globais"
+# Pacotes Base
+sudo pacman -S --noconfirm base base-devel linux-lts linux-firmware sof-firmware networkmanager network-manager-applet grub grub-btrfs btrfs-progs efibootmgr net-tools xf86-video-ati ffmpegthumbnailer ffmpegthumbs alsa-utils alsa-firmware a52dec faac flac jasper lame libdca libmpeg2 libtheora libvorbis libxv wavpack x264 x265 xvidcore xorg xorg-server xorg-apps xorg-xinit xorg-drivers xdg-desktop-portal xdg-desktop-portal-gtk texinfo
+# Sistema
+sudo pacman -S --noconfirm dmenu clipmenu dunst polybar feh peek picom nitrogen sxhkd wmname rofi reflector flatpak neofetch htop ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer ufw gufw libinput thunar
+echo -n "Finalizado"
+# Internet
+echo -n "Instalando Ambiente de Inteenet"
+sudo pacman -S --noconfirm vivaldi vivaldi-ffmpeg-codecs firefox transmission-gtk
+echo -n "Finalizado"
+# Cliente Mensageiros
+echo -n "Instalando Clientes de Mensageria"
+sudo pacman -S --noconfirm discord telegram-desktop
+echo -n "Finalizado"
+# Jogos
+echo -n "Instalando setup para jogos"
+sudo pacman -S --noconfirm steam mangohud lib32-mangohud lutris
+echo -n "Finalizado"
+# Editor de texto
+echo -n "Instalando editor de texto"
+sudo pacman -S --noconfirm emacs emacs-lua-mode
+echo -n "Finalizado"
+# Multimidia
+echo -n "Instalando ferramentas de multimidia"
+sudo pacman -S --noconfirm obs-studio yt-dlp audacious vlc cheese libcheese
+echo -n "Finalizado"
+# Ferramentas Gráficas
+echo -n "Instalando ambiente de design"
+sudo pacman -S --noconfirm krita gimp inkscape blender
+echo -n "Finalizado"
+# Suíte de Escritório
+echo -n "Instalando utilitarios de escritório"
+sudo pacman -S --noconfirm okular
+echo -n "Finalizado"
+# Suíte compactação de arquivos
+echo -n "Instalando utilitarios de empacotamento"
+sudo pacman -S --noconfirm ark unzip
+echo -n "Finalizado"
+# Desenvolvimento
+echo -n "Instalando Ambiente de Desenvolvimento"
+sudo pacman -S --noconfirm git github-cli nodejs npm yarn pnpm pm2 apache php php-apache phpmyadmin sqlite lib32-sqlite python python-pip docker jdk-openjdk
+echo -n "Finalizado"
+# Utiliarios
+echo -n "Instalando utiliarios do sistema"
+sudo pacman -S --noconfirm vim kitty alacritty zsh starship conky flameshot copyq
+# Download oh my zsh +  plugins
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cd ~/.oh-my-zsh/plugins
+git clone https://github.com/zsh-users/zsh-completions.git
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/zsh-users/zsh-autosuggestions.git
+cd ~/
+echo -n "Finalizado"
+# Apps do usuário do AUR
+mkdir ~/aur && cd ~/aur
+git clone https://aur.archlinux.org/yay.git
+cd yay && makepkg -sic --skippgpcheck
+cd ..
+git clone https://aur.archlinux.org/ncurses5-compat-libs.git
+cd ncurses5-compat-libs && makepkg -sic --skippgpcheck
+cd ..
+git clone https://aur.archlinux.org/local-by-flywheel-bin.git
+cd local-by-flywheel-bin && makepkg -sic --skippgpcheck
+cd ~/
+
+yay -Syu onlyoffice-bin spotify visual-studio-code-bin mockoon-bin obsidian-bin postman-bin
+# Finalizado
+sudo pacman-key --refresh-keys 
+sudo pacman -Syu && sudo pacman -Rs $(pacman -Qqdt)
+echo -n "Setup completo"
+reboot
